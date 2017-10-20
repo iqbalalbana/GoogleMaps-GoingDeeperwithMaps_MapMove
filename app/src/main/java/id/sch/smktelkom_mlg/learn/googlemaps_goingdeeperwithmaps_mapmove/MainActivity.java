5f513e257353dcf3d68267a8b77cd99a965c47e5
Iqbal Albana <iqbalalbana16@gmail.com>
2017-10-20 16:41:39 +0700

    - Initial Commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/googlemaps_goingdeeperwithmaps_mapmove/MainActivity.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.learn.googlemaps_goingdeeperwithmaps_mapmove;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
